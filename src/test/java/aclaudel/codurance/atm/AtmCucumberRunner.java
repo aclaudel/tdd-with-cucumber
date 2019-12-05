c2e22143ea5713522b118589f24e775d301d3c7f
aclaudel <arnaud.claudel@codurance.com>
2019-12-05 10:32:33 +0000

    strict mode and updated pictures

--- src/test/java/aclaudel/codurance/atm/AtmCucumberRunner.java
+++ src/test/java/aclaudel/codurance/atm/AtmCucumberRunner.java
@@ -8,5 +8,6 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = "pretty",
+        strict = true,
         tags = "(@delivery and (@wip or @done))")
 public class AtmCucumberRunner { }
