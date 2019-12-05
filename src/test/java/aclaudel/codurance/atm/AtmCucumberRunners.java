61f3e5949ac75d1c225ec1a57a440ecf76b0fc0b
aclaudel <arnaud.claudel@codurance.com>
2019-12-05 16:15:11 +0000

    more runners added

---
+++ src/test/java/aclaudel/codurance/atm/AtmCucumberRunners.java
@@ -0,0 +1,30 @@
+package aclaudel.codurance.atm;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = "pretty",
+        strict = true)
+public class AtmCucumberRunners {
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = "pretty",
+            strict = true,
+            tags = "(@delivery and (@done or @wip))")
+    public static class CurrentDeliveryRunner {}
+
+    @RunWith(Cucumber.class)
+    @CucumberOptions(
+            plugin = "pretty",
+            strict = true,
+            tags = "not @todo")
+    public static class AllButTodoDeliveryRunner {}
+
+}
+
+
